c0cb257825a241140ff23a8fb2aadff7a95b8866
alimabsoute <alimabsoute@gmail.com>
2014-10-23 22:34:34 -0400

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Ali Mabsoute on 10/23/14.
+//  Copyright (c) 2014 Ali Mabsoute. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Ali Mabsoute on 10/23/14.
+//  Copyright (c) 2014 Ali Mabsoute. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
